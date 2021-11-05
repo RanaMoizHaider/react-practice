8dc9ecb752704ae1babda217367abda2b72f438c
Moiz Haider <moizhaiderali74@gmail.com>
2021-11-05 15:12:58 +0500

    Update Counter.jsx

--- src/components/examples/Counter.jsx
+++ src/components/examples/Counter.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Counter = () => {
+
+    let count = 5;
     return ( 
         <div>
             <h5>My Counter</h5>
+            <button>-</button> {count} <button>+</button> <br />
+            Current count value is {count}
+
         </div>
      );
 }
