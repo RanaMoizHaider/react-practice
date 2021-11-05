588e257a16cdca82a6ff29e11b4229a37306ea72
Moiz Haider <moizhaiderali74@gmail.com>
2021-11-05 15:11:16 +0500

    Creating Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Counter from './components/examples/Counter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello Section B</h2>
+      < Counter />
+      < Counter />
+      < Counter />
+      < Counter />
     </div>
   );
 }
---
+++ src/components/examples/Counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Counter = () => {
+    return ( 
+        <div>
+            <h5>My Counter</h5>
+        </div>
+     );
+}
+ 
+export default Counter;
